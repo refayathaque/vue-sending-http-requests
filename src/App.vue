b3c8f6ebe8437fa76e26d67e7a10ac54e7d09126
Refayat Haque <rhaque@pyramidsystems.com>
2020-10-19 00:21:41 -0400

    Oct 19 021

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,8 @@
     <input id="greeting" name="greeting" type="text" v-model="greeting" />
     <button type="submit">Send this to AWS Lambda</button>
   </form>
+  <button type="button" @click="fetchFromAWSLambda">Fetch from AWS Lambda</button>
+  <p>{{ returnedFromAWSLambda }}</p>
 </template>
 
 <script>
@@ -13,19 +15,38 @@ import { awsResources } from './aws'
 export default {
   data() {
     return {
-      greeting: ''
+      greeting: '',
+      returnedFromAWSLambda: ''
     }
   },
   methods: {
-    submitForm() {
+    async fetchFromAWSLambda() {
+      console.log('hello')
+      const response = await fetch(awsResources.apiGatewayEndpoint)
+      // don't need config object for GET, because it's the default and since we're not sending anything we don't need body and therefore don't need headers
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        this.returnedFromAWSLambda = data.body
+      } else {
+        console.log('Go fix something in AWS')
+      }
+    },
+    async submitForm() {
       console.log(this.greeting)
-      fetch(awsResources.apiGatewayEndpoint, {
+      const response = await fetch(awsResources.apiGatewayEndpoint, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({ greeting: this.greeting }),
       })
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+      } else {
+        console.log('Go fix something in AWS')
+      }
     }
   }
 }
